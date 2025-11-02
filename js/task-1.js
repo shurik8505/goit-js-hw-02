ed86cff92ef4f0edc35ba08ad53b4c8f4e11f65f
Oleksandr <shurik8505@gmail.com>
2025-11-02 14:38:42 +0100

    add

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+function slugify(title) {
+const lowercaseTitle = title.toLowerCase();
+const slug = lowercaseTitle.split(" ").join("-");
+return slug;
+}
+
+console.log(slugify("Arrays for beginners")); 
+console.log(slugify("English for developer")); 
+console.log(slugify("Ten secrets of JavaScript")); 
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
